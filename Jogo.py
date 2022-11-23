b7cc82f93dc78646507681bc85d9f149455921d3
heber3000 <heberlevy@gmail.com>
2022-11-23 15:36:09 -0300

    Adicionado Biblioteca tkinter e random e montagem de funções

--- Jogo.py
+++ Jogo.py
@@ -4,41 +4,14 @@ import tkinter as tk
 
 janela = tk.Tk()
 janela.geometry("400x300")
-janela.title("Pedra,Papel e Tesoura")
-
-PONTOS_USUARIO = 0
-PONTOS_COMPUTADOR = 0
-ESCOLHA_USUARIO = ""
-ESCOLHA_COMPUTADOR = ""
+janela.title('Pedra,Papel e Tesoura Jogo')
 
 
-def escolha_um_numero(escolha):
-    opcoes = {'pedra(rock)': 0, 'papel(paper)': 1, 'tesoura(scissors)': 2}
-    return opcoes[escolha]
+USUARIO_PONTOS = 0
+COMP_PONTOS = 0
+USUARIO_ESCOLHA = ""
+COMP_ESCOLHA = ""
 
-
-def numero_de_escolha(escolha):
-    opcoes = {0: 'pedra(rock)', 1: 'papel(paper)', 'tesoura(scissors)': 2}
-    return opcoes[escolha]
-
-
-def escolha_aleatoria_do_computador():
-    return random.choice(['pedra(rock)', 'papel(paper)', 'tesoura(scissors)'])
-
-
-def resultado(escolha_humana, escolha_computador):
-    global PONTOS_USUARIO
-    global PONTOS_COMPUTADOR
-    usuario = escolha_um_numero(escolha_humana)
-    comp = escolha_computador(escolha_computador)
-    if usuario == comp:
-        print('Empate')
-    elif (usuario -comp) % 3 == 1:
-        print('Venceu')
-        PONTOS_USUARIO += 1
-    else:
-        print('Computador Venceu')
-        PONTOS_COMPUTADOR += 1
 
 
 
